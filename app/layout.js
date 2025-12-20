3145a1e5205cf0c53254c4220824be8741311ac4
pablomendo <138833211+pablomendo@users.noreply.github.com>
2025-12-20 11:45:52 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,16 @@
+export const metadata = {
+  title: 'HotCars',
+  description: 'Red de Stock Profesional',
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <head>
+        <title>HotCars</title>
         <script src="https://cdn.tailwindcss.com"></script>
       </head>
-      <body style={{ margin: 0, backgroundColor: '#020617' }}>
+      <body style={{ margin: 0, backgroundColor: '#020617', color: 'white' }}>
         {children}
       </body>
     </html>
