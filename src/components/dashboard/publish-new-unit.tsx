30403cf3974551b2c3b6af40ad52361e85261785
Pablo Mendo <mendo@MacBook-Pro-de-Pablo.local>
2026-01-11 17:33:56 -0300

    Agregando el componente final publish-new-unit

---
+++ src/components/dashboard/publish-new-unit.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { Plus } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+export function PublishNewUnit() {
+  return (
+    <Button className="bg-[#A6C94A] hover:bg-[#8eb13d] text-black font-semibold">
+      <Plus className="mr-2 h-4 w-4" /> Publicar Unidad
+    </Button>
+  );
+}
