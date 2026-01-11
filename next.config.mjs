d30625028f9a118a30d5633a2452a5621fdbea4b
Pablo Mendo <mendo@MacBook-Pro-de-Pablo.local>
2026-01-11 16:34:53 -0300

    Subiendo next.config.mjs limpio y funcional

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'placehold.co', pathname: '/**' },
+      { protocol: 'https', hostname: 'images.unsplash.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'picsum.photos', pathname: '/**' },
+      { protocol: 'https', hostname: 'images.jazelc.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'i.ibb.co', pathname: '/**' }
+    ],
+  },
+};
+export default nextConfig;
