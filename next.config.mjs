d767e5165f33f02f79fd6ef6460bee1fb4510c0c
Pablo Mendo <mendo@MacBook-Pro-de-Pablo.local>
2026-01-12 17:15:55 -0300

    Fix: Creado placeholder-data y actualizado tsconfig

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,5 @@
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
-  /* Esto ayuda a encontrar los archivos en src */
-  experimental: {
-    externalDir: true
-  }
 };
 export default nextConfig;
