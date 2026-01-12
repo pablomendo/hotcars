0a82ea716d209edffb658c2d4dad87ce4714cc9a
Pablo Mendo <mendo@MacBook-Pro-de-Pablo.local>
2026-01-12 16:52:35 -0300

    Fix: actualizando configuracion de entrada

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,5 @@
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'placehold.co', pathname: '/**' },
-      { protocol: 'https', hostname: 'images.unsplash.com', pathname: '/**' },
-      { protocol: 'https', hostname: 'picsum.photos', pathname: '/**' },
-      { protocol: 'https', hostname: 'images.jazelc.com', pathname: '/**' },
-      { protocol: 'https', hostname: 'i.ibb.co', pathname: '/**' }
-    ],
-  },
 };
 export default nextConfig;
