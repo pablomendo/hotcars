39481837f2daaaae49a7f3a85e73628884af1706
Pablo Mendo <pablomendo@Pablos-MacBook-Pro.local>
2026-03-11 00:55:39 -0300

    fix: remove eslint config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // Evita que el deploy falle por errores de TypeScript
     ignoreBuildErrors: true,
   },
 };
